b124e17363484f2ab5f1cf910941d19769f14427
Marouane El Madani <elmadani.marouane@gmail.com>
2018-06-03 01:59:33 +0200

    Tentative de forcing HTTPS

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@
     return view('welcome');
 }); */
 
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
+
 header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
